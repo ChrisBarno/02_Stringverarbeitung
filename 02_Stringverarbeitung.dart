2ef927a1e2f1177d125280b458f3ec259edb992b
ChrisBarno <67658251+ChrisBarno@users.noreply.github.com>
2020-07-07 12:38:03 +0200

    Create 02_Stringverarbeitung.dart

---
+++ 02_Stringverarbeitung.dart
@@ -0,0 +1,9 @@
+main(){
+  String meinName = "Kiwi";
+  print("Der String ist $meinName.");
+  double zahl = 2.3;
+  String cast = zahl.toString();
+  print("Irgendeine Zahl: $cast.");
+  int laenge = meinName.length;
+  print("Die Länge des Strings ist $laenge.");
+}
